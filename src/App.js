3cfa0c5146e180573bbfe2efa9ae5bb8c4e09338
merve.arslan <merve_arslann_07@hotmail.com>
2021-03-23 21:42:20 +0300

    App files deleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
 import Grid from './components/grid/grid';
 
 function App() {
   return (
-    // <div>
-    <Grid />
-    // </div>
+    <>
+      <Grid />
+    </>
   );
 }
 
